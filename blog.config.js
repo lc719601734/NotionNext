4a29d87cbe658f61d9b71a5b73773f10e4314ba5
lc719601734 <76467102+lc719601734@users.noreply.github.com>
2026-02-11 15:11:56 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,10 +15,10 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '大连氢翼', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Hydrogen Ltd🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://dlqy.com.cn', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'DLQY,Hydrogen', // 网站关键词 英文逗号隔开
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'PaisoTech', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || 'Hydrogen Compressor and Refueling🍚', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.paisotech.com', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Hydrogen Compressor,Hydrogen Refueling', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
@@ -65,7 +65,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是氢能设备商, Hi，我是氢能方案解决商,欢迎来到🎉',
+    'Hi，我是氢能设备商, Hi，我是氢能方案解决商,欢迎到来🎉',
   // uuid重定向至 slug
  UUID_REDIRECT: process.env.UUID_REDIRECT || false
 }
