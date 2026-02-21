4c22cbb3d93be18251ce2ca312e73d4c6946b1df
lc719601734 <719601734@qq.com>
2026-02-21 09:34:18 +0800

    hero photo

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -21,7 +21,7 @@ const CONFIG = {
   STARTER_HERO_BUTTON_2_ICON: '', // 英雄区按钮2的图标，不需要则留空
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
-  STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/paisotech-hero.jpg', // 产品预览图 ，默认读取public目录下图片
+  STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image-paiso', // 产品预览图 ，默认读取public目录下图片
   //STARTER_HERO_BANNER_IMAGE: '/images/starter/hero/paisotech-banner.jpg', // hero区下方的全宽图
 
   // 顶部右侧导航暗流
