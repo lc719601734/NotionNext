df720f551f0caba752360a3850f042f9892efb6b
lc719601734 <719601734@qq.com>
2026-02-24 07:53:56 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -245,7 +245,7 @@ const CONFIG = {
   STARTER_CONTACT_EMAIL_TEXT: '15524855312@139.com',
 
   // 嵌入外部表单
-
+  STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://forms.office.com/r/va4UUgdhsK?origin=lprLink'
   //STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/yfctc7', // 基于NoteForm创建，将留言数据存在Notion中
   //   自定义留言表单，以下几个配置暂时废弃
   //   STARTER_CONTACT_MSG_TITLE: '向我们留言',
